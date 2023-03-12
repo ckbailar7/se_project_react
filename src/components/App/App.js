7488def3b453dc800ae6d2c1a2f3a424aa4ad031
Jared Girvan <tkdinst.429897@gmail.com>
2023-03-12 16:10:59 -0400

    Setup/Cleanup

---
+++ src/components/App/App.js
@@ -0,0 +1,19 @@
+import Header from "../Header/Header";
+import Main from "../Main/Main";
+import Footer from "../Footer/Footer";
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
+import ItemModal from "../ItemModal/ItemModal";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+      <ModalWithForm />
+      <ItemModal />
+    </>
+  );
+}
+
+export default App;
